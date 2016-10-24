f179e1c2880dae57df099926fa57a494c74af9d6
fluttershy <fluttershydeveloper@gmail.com>
2016-10-24 02:49:24 +0200

    xUnit 2.1.0 installed

--- TestDrivenDevelopment_Examples/TestDrivenDevelopment_Examples/Program.cs
+++ TestDrivenDevelopment_Examples/TestDrivenDevelopment_Examples/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TestDrivenDevelopment_Examples
+namespace MyLittlePrograms.TDD.Examples
 {
     class Program
     {
---
+++ TestDrivenDevelopment_Examples/TestDrivenDevelopment_Examples/Specifications/ProgramSpecifications.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace MyLittlePrograms.TTD.Examples.Specifications
+{
+    public class ProgramSpecifications
+    {
+
+        [Fact]
+        public void ShouldBehavior()
+        {
+            // GIVEN
+
+            // WHEN
+
+            // THEN
+            Assert.True(false);
+        }
+
+    }
+}
